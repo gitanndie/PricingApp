abf9a813161c1c2e0fa93c823aea9459a2e160b4
Anndie Gonzalez <anndiegonzalez@gmail.com>
2020-08-20 15:31:02 -0500

    subiendo carpetas

--- src/components/PricingContainer.jsx
+++ src/components/PricingContainer.jsx
@@ -1,45 +1,26 @@
-import React, { Component } from "react";
-import PricingDetails from "./PricingDetails";
-
+import React, { Component } from 'react';
+import PricingDetails from './PricingDetails'
 class PricingContainer extends Component {
-
-    constructor(props){
+    constructor (props){
         super(props);
         this.state = {
-            card1: [
-                "10 users included",
-                "2 GB of storage",
-                "Email support", 
-                "Help center access",
-            ],
-            card2: [
-                "20 users included",
-                "10 GB of storage",
-                "Priority email support",
-                "Help center access",
-            ],
-            card3: [
-                "30 users included",
-                "15 GB of storage",
-                "Phone and email support",
-                "Help center access",
-            ]
-
+        card1: ["10 users included", "2 gb of storage", "Email support", "Help center access"],
+        card2: ["20 users included", "10 gb of storage", "Priority emaol support","Help center access"],
+        card3: ["30 users included", "gb of storage", "Email support", "Help center access"],
         }
     }
-    render(){
 
-    
-
-return (
-        <div className="container">
-        <div className="card-deck mb-3 text-center">
-        < PricingDetails />
+    render() {
+        return (
+        <div className='container'>
+            <div className='card-deck mb-3 text-center'>
+            <PricingDetails textoButton="Sing up for free" precio="0" plan="Free" items={this.state.card1}/>
+            <PricingDetails textoButton="Get Started" precio="25" plan="Pro" items={this.state.card2}/>
+            <PricingDetails textoButton="Contact Us" precio="29" plan="Enterprise" items={this.state.card3}/>
+            </div>
         </div>
-        </div>
-        
-    )
-};
-};
+        );
+    }
+    };
 
 export default PricingContainer;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PricingPage from './containers/PricingPage';
-import $ from 'jquery';
-import Popper from 'popper.js';
+/* import $ from 'jquery';
+import Popper from 'popper.js'; */
 import 'bootstrap/dist/css/bootstrap.min.css';
 /*
 ReactDOM.render( 
